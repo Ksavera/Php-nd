fd3caea378e95b3fcad8645f0e8663cbf35a93de
arevask@gmail.com <arevask@gmail.com>
2023-09-04 15:53:55 +0300

    jurgitai

--- 2023-09-04_fileManager/filesystem.php
+++ 2023-09-04_fileManager/filesystem.php
@@ -5,21 +5,51 @@ if(is_dir('./directories')) {
   echo "wrong path or something";
 }
 
-
 echo '<pre>';
 $directories = scandir('./directories');
+
 unset($directories[0]);
 unset($directories[1]);
+
+// Define the root directory (initial directory)
+$initial_directory = './directories'; // Change this to the actual root directory
+
+// Extract the directory path from the URL parameter (if provided)
+if (isset($_GET['directory'])) {
+    $directory_param = $_GET['directory'];
+    
+    // Sanitize the directory_param to prevent directory traversal attacks
+    $directory_param = preg_replace('/\.\.\//', '', $directory_param);
+    
+    // Define the current directory based on the parameter
+    $current_directory = $initial_directory . '/' . $directory_param;
+} else {
+    // If no directory parameter is provided, use the initial directory
+    $current_directory = $initial_directory;
+}
+
+// Check if the current directory exists and is a directory
+if (is_dir($current_directory)) {
+    // Directory exists, proceed with listing its contents
+    echo '<p style="color: red">Current Directory is a directory</p>';
+    echo '<pre>';
+    $directories = scandir($current_directory);
+    unset($directories[0]);
+    unset($directories[1]);
+} else {
+    // Directory does not exist or is not a directory
+    echo "Wrong path or something";
+    // You can handle the error here as needed
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <title>file system manager</title>
+    <!-- ... (head section remains the same) ... -->
   </head>
   <body>
   
@@ -34,16 +64,20 @@ unset($directories[1]);
         </tr>
       </thead>
       <tbody>
-        <?php 
-          foreach($directories as $directory) 
-          {
-            echo "
-            <tr>
-              <td>\/</td>
-              <td>$directory</td>
-            </tr>";
-          }
-        ?>
+      <?php 
+      if (isset($_GET['directory']) && realpath($current_directory) != realpath($initial_directory)): ?>
+        <tr>
+          <td colspan="5" class="name"><a href="?directory=<?= urlencode($_GET['directory']) . '/..' ?>">...</a></td>
+        </tr>
+      <?php endif; ?>
+      
+      <?php foreach($directories as $directory): ?>
+        <tr>
+          <td>\/</td>
+          <td><a href="?directory=<?= urlencode($directory) ?>"><?php echo $directory ?></a></td>
+        </tr>
+      <?php endforeach; ?>
+        
       </tbody>
     </table>
   </body>
