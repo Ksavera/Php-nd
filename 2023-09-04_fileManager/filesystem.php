8ac0e1c510a7eb95df1c151512780962a64abeea
arevask@gmail.com <arevask@gmail.com>
2023-09-04 22:38:01 +0300

    linkai

--- 2023-09-04_fileManager/filesystem.php
+++ 2023-09-04_fileManager/filesystem.php
@@ -1,46 +1,6 @@
-<?php
-if(is_dir('./directories')) {
-  echo '<p style="color: red">Directories is a directory</p>';
-} else {
-  echo "wrong path or something";
-}
-
+<?php 
 echo '<pre>';
 $directories = scandir('./directories');
-
-unset($directories[0]);
-unset($directories[1]);
-
-// Define the root directory (initial directory)
-$initial_directory = './directories'; // Change this to the actual root directory
-
-// Extract the directory path from the URL parameter (if provided)
-if (isset($_GET['directory'])) {
-    $directory_param = $_GET['directory'];
-    
-    // Sanitize the directory_param to prevent directory traversal attacks
-    $directory_param = preg_replace('/\.\.\//', '', $directory_param);
-    
-    // Define the current directory based on the parameter
-    $current_directory = $initial_directory . '/' . $directory_param;
-} else {
-    // If no directory parameter is provided, use the initial directory
-    $current_directory = $initial_directory;
-}
-
-// Check if the current directory exists and is a directory
-if (is_dir($current_directory)) {
-    // Directory exists, proceed with listing its contents
-    echo '<p style="color: red">Current Directory is a directory</p>';
-    echo '<pre>';
-    $directories = scandir($current_directory);
-    unset($directories[0]);
-    unset($directories[1]);
-} else {
-    // Directory does not exist or is not a directory
-    echo "Wrong path or something";
-    // You can handle the error here as needed
-}
 
 
 
@@ -48,37 +8,47 @@ if (is_dir($current_directory)) {
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- ... (head section remains the same) ... -->
-  </head>
-  <body>
-  
-    <table class="table">
-      <thead>
-        <tr>
-          <th>\/</th>
-          <th>Name</th>
-          <th>Size</th>
-          <th>Modified</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-      <?php 
-      if (isset($_GET['directory']) && realpath($current_directory) != realpath($initial_directory)): ?>
-        <tr>
-          <td colspan="5" class="name"><a href="?directory=<?= urlencode($_GET['directory']) . '/..' ?>">...</a></td>
-        </tr>
-      <?php endif; ?>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container mt-5">
+  <?php 
+// file_get_contents('http://localhost/php_nd/2023-09-04_fileManager')
+if(is_dir('directories')) {
+  echo '<p style="color:red">This is directory</p>';
+} else {
+  echo 'smth wrong';
+}
+?>
+     <table class="table">
+    <thead>
+      <tr>
+        <th><input type="checkbox" class="form-check-input"></th>
+        <th>Name</th>
+        <th>Size</th>
+        <th>Modified</th>
+        <th>Actions</th>
+      </tr>
       
-      <?php foreach($directories as $directory): ?>
-        <tr>
-          <td>\/</td>
-          <td><a href="?directory=<?= urlencode($directory) ?>"><?php echo $directory ?></a></td>
-        </tr>
-      <?php endforeach; ?>
-        
-      </tbody>
-    </table>
-  </body>
+    </thead>
+    <tbody>
+      <?php foreach($directories as $directory){
+        echo " <tr>
+        <td><input type='checkbox' class='form-check-input'></td>
+        <td><a href='./directories/$directory'>$directory</a></td>
+        <td><a href='#'><?php include 'directory_size.php'; ?></a></td>
+      </tr>";
+      } ?>
+     
+    </tbody>
+  </table>
+  </div>
+ 
+</body>
 </html>
