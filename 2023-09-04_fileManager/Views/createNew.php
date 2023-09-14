c45cae67a3122b6f0e3c32bf89b75d6d25df91bd
arevask@gmail.com <arevask@gmail.com>
2023-09-14 18:20:05 +0300

    added home, fixed Routes.

--- 2023-09-04_fileManager/Views/createNew.php
+++ 2023-09-04_fileManager/Views/createNew.php
@@ -9,59 +9,39 @@ $filePath = $path . '/' . $fileName;
 
 if ($fileType === "file" && $fileName) {
     if (array_key_exists('extension', $pathPart)) {
-        if ($pathPart['extension'] === '') {
-            echo '<p class="center-text">Must enter an extension to create a file.</p>';
-        } else {
             $myfile = fopen($filePath, "w") or die("Unable to open file!");
             fclose($myfile);
             header('Location: index.php?path=' . $path);
-        }
+        
     } else {
-        echo 'Invalid file name format. Make sure to include an extension.';
+        
+        echo '<div class="alert alert-danger mt-5" role="alert">Invalid file name format. Make sure to include an extension.</div>';
     }
 } elseif ($fileType === 'folder' && $fileName) {
     if (strpos($fileName, '.') !== false) {
-        echo '<p class="center-text">Must enter folder name without extension.</p>';
+        echo '<div class="alert alert-danger mt-5" role="alert">You must enter folder name without extension.</div>';
     } else {
         mkdir($filePath);
         header('Location: index.php?path=' . $path);
     }
 }
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <title>new Item</title>
     <style>
-        body {
-
+        .container2 {  
             margin: 25vh;
-        }
-
-        .container {
             width: 500px;
             border-radius: 10px;
         }
         .center-text {
             text-align: center !important;
             color: red;
-}
+        }
     </style>
-</head>
 
-<body class="bg-light">
-    <div class="container bg-white py-3">
+    <div class="container2 bg-white my-5 p-3">
         <h5 class=""><i class="newItem bi bi-plus-square mx-1"></i>Create new Item</h5>
         <hr>
-        <p>Item type:</p>
+        
         <form method="POST">
             <div class="d-flex gap-4">
                 <div class="form-check">
@@ -88,6 +68,3 @@ if ($fileType === "file" && $fileName) {
 
 
     </div>
-</body>
-
-</html>
--- 2023-09-04_fileManager/index.php
+++ 2023-09-04_fileManager/index.php
@@ -27,13 +27,10 @@ function removeRecursively($dir)
         rmdir($dir);
     }
 }
-$disallowedFiles = ["index.php", "receiveFormData1.php", "uploadForm.php", "uploads"];
+$disallowedFiles = ["index.php", "receiveFormData.php", "uploadForm.php", "createNEw.php", "uploads"];
 $deleteMode = isset($_GET['action']) && $_GET['action'] === 'delete' && isset($_GET['file']);
 if ($deleteMode) {
     $fileToDelete = $_GET['file']; // Get the file to be deleted
-
-    // Disallowed files
-    
 
     if (!in_array($fileToDelete, $disallowedFiles)) {
         $deleteitem = $path . '/' . $fileToDelete; // Use the correct file name
@@ -68,6 +65,8 @@ if (isset($_POST['deleteSelected'])) {
         exit;
     }
 }
+
+$page = isset($_GET['page']) ? $_GET['page'] : false;
 ?>
 
 <!DOCTYPE html>
@@ -90,183 +89,39 @@ if (isset($_POST['deleteSelected'])) {
             cursor: pointer;
             color: black;
         }
+     
     </style>
 </head>
-
+ 
 <body class="bg-secondary-subtle">
-    <header class=" border pt-2 bg-light shadow-sm bg-body">
-        <div class="container d-flex justify-content-between">
-            <div class="d-flex">
-                <h5>H3K Demo</h5>
-                <i class="bi bi-house-fill px-2 text-primary"></i>
-            </div>
-            <nav>
-                <ul class="d-flex gap-5 list-unstyled text-muted">
-                    <li class="upload">
-                        <a href="uploadForm.php" class="link-secondary link-underline link-underline-opacity-0">
-                            <i class=" bi bi-cloud-arrow-up"></i>
-                            <span>Upload</span>
-                        </a>
-                    </li>
-                    <li class="newItem">
-                    <a href="newItem.php?path=<?php echo $path; ?>" class="link-secondary link-underline link-underline-opacity-0">
-                         <i class="bi bi-plus-square"></i>  
-                            <span>New Item</span>
-                        </a>
-                        </li> 
-                    <li><i class="settings bi bi-gear"></i>Settings</li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-
+   <?php include './Views/snippets/Header.php'; ?>
+    
+    <main>
     <div class="container">
-    <form method="POST">
-        <table class="table mt-5">
-            <thead>
-                <tr>
-                    <th><input type="checkbox" onclick="selectAll(event)" class="form-check-input"></th>
-                    <th>Name</th>
-                    <th>Size</th>
-                    <th>Modified</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <?php
-
-                foreach ($files as $file) {
-                    $fileToDelete = isset($_POST['selectedFiles']) ? $_POST['selectedFiles'] : [];
-
-//--------------------------edit------------------------------------------------------------------
-
-                    $editMode = isset($_GET['action']) && $_GET['action'] === 'edit' && isset($_GET['file']) && $_GET['file'] === $file;
-                    if ($editMode) {
-                        $editFile = pathinfo($_GET['file']);
-                        $editFileName = pathinfo($_GET['file'], PATHINFO_FILENAME);
-
-                        $form = "<form method='POST'>
-                        <div class='input-group'>
-                            <input type='text' name='newName' value='$editFileName' class='form-control'>
-                            <button class='btn btn-primary'>Submit</button>
-                        </div>
-                    </form>";
-                    } else {
-                        $form = "";
-                    }
-
-                    if ($editMode && isset($_POST['newName'])) {
-                        $newName = $_POST['newName'] . '.' . pathinfo($_GET['file'], PATHINFO_EXTENSION);
-                        rename("$path/$file", "$path/$newName");
-                        $editedFileDirectory = dirname("$path/$file");
-                        header("Location: ?path=$editedFileDirectory");
-                        exit;
-                    }
-                   
-
-//------------------------ikonos prie failu---------------------------------------------------------
-                    $kintamasis = pathinfo($file);
-                    if (array_key_exists('extension', $kintamasis)) {
-                        if ($kintamasis['extension'] === "php") {
-                            $icon_class = "bi bi-filetype-php";
-                        } else if ($kintamasis['extension'] === "md") {
-                            $icon_class = "bi bi-filetype-md";
-                        } else if ($kintamasis['extension'] === "txt") {
-                            $icon_class = "bi bi-filetype-txt";
-                        } else if ($kintamasis['extension'] === "html") {
-                            $icon_class = "bi bi-filetype-html";
-                        } else if ($kintamasis['extension'] === "jpg") {
-                            $icon_class = "bi bi-image";
-                        } else if ($kintamasis['extension'] === "png") {
-                            $icon_class = "bi bi-image";
-                        } else if ($kintamasis['extension'] === "git") {
-                            $icon_class = 'bi bi-github';
-                        } else if ($kintamasis['extension'] === "php") {
-                            $icon_class = 'bi bi-filetype-php';
-                        } else if ($kintamasis['extension'] === "pdf") {
-                            $icon_class = 'bi bi-file-earmark-pdf-fill';
-                        } else if ($kintamasis['extension'] === "gif") {
-                            $icon_class = 'bi bi-filetype-gif';
-                        } else if ($kintamasis['extension'] === "mp3") {
-                            $icon_class = 'bi bi-file-earmark-music';
-                        } else if ($kintamasis['extension'] === "mp4") {
-                            $icon_class = 'bi bi-play-circle';
-                        } else {
-                            $icon_class = "";
-                        }
-                    } else {
-                        $icon_class = "bi bi-folder2";
-                    }
-
-// --------------------------------rodo size arba folder----------------------------------------------
-                    $realfile = "$path/$file";
-                    $isDirectory = is_dir($realfile);
-                    
-                    $size = filesize($realfile);
-                    if ($size >= 1048576) {
-                        $size = round($size / 1024) . " mb";
-                    } else if ($size >= 1024) {
-                        $size = round($size / 1024) . " kb";
-                    } else {
-                        $size = $size . " b";
-                    }
-
-//------------------------------------- Size or Folder--------------------------------------------------
-
-                    $FolderOrEmpty = ($file === "..") ? "" : "Folder";
-                    $isFolderOrSize = is_file($realfile) ? $size : $FolderOrEmpty;
-//-------------------------------- Modifikuoto failo data-----------------------------------------------
-
-                    $laikas = ($file !== "..") ? date("d/m/Y h:i A", filemtime($realfile)) : "";
-//---------------------------------------icons------------------------------------------------------------
-
-                    $delete_icon = ($file === ".."  && !$deleteMode || $file === "uploadForm.php" || $file === "receiveFormData1.php" || $file === "uploads") ? "" : "bi bi-trash";
-
-                    $edit_icon = ($file === ".." && !$editMode || $file === "uploadForm.php" || $file === "receiveFormData1.php" || $file === "uploads" ) ? "" : "bi bi-pencil-square";
-
-//----------------------grizineja atgal ir pirmyn per viena direktorija----------------------------------
-
-                    if ($file === ".." && $path !== ".") {
-                        $link = "<a href='?path=" . dirname($path) . "'><i class='bi bi-arrow-left-circle-fill'></i></a>";
-                    } else {
-                        $link = "<a href='?path=" . "$path/$file" . "'>$file</a>";
-                    }
-
-//-------------------------------- nerodo directorijos . ir index.php -----------------------------------
-
-                    if ($file !== "." && $file !== "index.php" && $file !== "newItem.php" && $file !== "receiveFormData1.php" && $file !== "uploadForm.php" ) {
-                        echo " <tr>
-        <td><input type='checkbox' class='form-check-input' name='selectedFiles[]' value='$file'></td>
-        <td>
-        <i class='$icon_class'></i>$link $form</td>
-        <td>$isFolderOrSize</td>
-        <td>$laikas</td>
-        <td>
-          <a href='?action=edit&path=$path&file=$file'><i class='$edit_icon'></i>$editMode</a>
-          <a href='?action=delete&path=$path&file=$file'><i class='$delete_icon'>$deleteMode</i></a>
-        </td>
-      </tr>";
-                    }
-                }
-                ob_end_flush(); // Flush the output buffer
-                ?>
-            </tbody>
-        </table>
-        <div class="mt-3">
-                <button type="submit" name="deleteSelected" class="btn btn-danger">Delete Selected</button>
-            </div>
-        </form>
+    <?php switch ($page) { 
+        case 'createNew':
+            include './Views/createNew.php';
+            break;
+        case 'upload':
+            include './Views/uploadForm.php';
+            break;
+       
+        default:
+            include './Views/Home.php';
+    }
+    ?>
+    </main>
     </div>
-  <script>
+    
+
+</body>
+<script>
       function selectAll(e) {
         const checkAll = document.querySelectorAll('input[type="checkbox"]');
         checkAll.forEach((el) => {
             el.checked = !el.checked;
         });
     }
-</script>  
-</body>
-
+</script> 
 
 </html>
